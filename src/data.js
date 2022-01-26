cab4ecaaa245689089a040a15791c6845ad1000e
Irina Abah <irin.bon@gmail.com>
2022-01-26 16:36:27 +0000

    code refactor

--- src/data.js
+++ src/data.js
@@ -3,91 +3,76 @@ const catsData = [
      "name":"Whiskers",
      "breed":"British Shorthair",
      "price": 480,
-     "image":""
   },
   {
     "name":"Cooper",
     "breed":"Abyssinian Cat",
     "price": 590,
-    "image":""
   },
   {
     "name":"Santiago",
     "breed":"Bengal Cat",
     "price": 500,
-    "image":""
   },
   {
     "name":"Charlie",
     "breed":"Bombay Cat",
     "price": 850,
-    "image":""
   },
   {
     "name":"Lucy",
     "breed":"Burmese Cat",
     "price": 877,
-    "image":""
   },
   {
     "name":"Leo",
     "breed":"Devon Rex Cat Breed",
     "price": 355,
-    "image":""
   },
   {
     "name":"Simba",
     "breed":"Himalayan Cat Breed",
     "price": 500,
-    "image":""
   },
   {
     "name":"Nala",
     "breed":"Manx Cat",
     "price": 700,
-    "image":""
   },
   {
     "name":"Kitty",
     "breed":"Savannah Cat Breed",
     "price": 768,
-    "image":""
   },
   {
     "name":"Milo",
     "breed":"Russian Blue Cat Breed",
     "price": 167,
-    "image":""
   },
   {
     "name":"Loki",
     "breed":"Siberian Cat Breed",
-    "price": 900,
-    "image":""
+    "price": 900
   },
   {
     "name":"Bella",
     "breed":"Turkish Angora Cat Breed",
     "price": 200,
-    "image":""
   },
   {
     "name":"Willow",
     "breed":"Siamese Cat Breed",
     "price": 450,
-    "image":""
   },
   {
     "name":"Oliver",
     "breed":"Ragamuffin Cat",
     "price": 300,
-    "image":""
   },
   {
     "name":"Luna",
     "breed":"Toyger Cat Breed",
     "price": 955,
-    "image":""
   }
 ] 
 
