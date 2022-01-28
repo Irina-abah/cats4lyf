61871e27e68f6a74490efe3569bbc0cfc18a6b77
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-01-28 15:19:30 +0000

    Add header and footer, update CSS

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../App.css"
+
+const Footer = () => {
+
+    return (
+        <footer>
+            <p>&#169; 2022 Cats4Lyf</p>
+            <div className="footer-links">
+                <ul>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms & Conditions</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
